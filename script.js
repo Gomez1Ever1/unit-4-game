ecab139a7117d59704bf27860072d0238f022dcc
Ever's PC <gomezsantiagoeverardo@gmail.com>
2019-10-11 23:32:01 -0700

    completed the js script of the game. need to add some bootstrap elements which i am unsure of

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
-var randNum = 0;
-var totalScore = 0;
+var newRandNum;
+var newScore;
+var totalScore;
 var crystalValues = {
     gem1: "", gem2: "", gem3: "", gem4: ""
 };
-
-function initialize() {
-    var newRandNum = getRandNum(19, 120);
+var wins = 0;
+var losses = 0;
+function getRandNum(min, max) {
+    return Math.floor(Math.random() * (max - min + .9)) + min;
+}
+function startGame() {
+    newRandNum = getRandNum(19, 120);
     $("#randomNumber").text(newRandNum);
     getRandomGem();
+    totalScore = 0;
+    $("#accScore").text(totalScore);
+    $("#winCounter").text(wins);
+    $("#lossCounter").text(losses);
 }
-function getRandNum(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-function changingText(variable, content) {
-    content.text = variable;
+function updateScore() {
+    totalScore = newScore + totalScore;
+    $("#accScore").text(totalScore);
 }
 function getRandomGem() {
     for (var newVal in crystalValues) {
         if (crystalValues.hasOwnProperty(newVal)) {
-            var gemNum = getRandNum(1, 20);
+            var gemNum = getRandNum(1, 12);
             crystalValues[newVal] = gemNum;
-            changingText(gemNum, newVal);
         };
     } console.log(crystalValues);
 }
-function boxClick(gem) {
-    var numAdd = gem + totalScore;
-    totalScore = numAdd;
-    console.log(totalScore)
+function boxClick(difNum) {
+    newScore = difNum;
+    return newScore;
 }
-initialize();
-$("#accScore").text(totalScore);
-$("#box1").on("click", boxClick(crystalValues.gem1));
+function isGameOver() {
+    if (totalScore > newRandNum) {
+        alert('Game Over');
+        losses++;
+        startGame();
+    }
+    else if (totalScore === newRandNum) {
+        alert("you win!");
+        wins++;
+        startGame();
+    }
+}
+startGame();
+$("#box1").on("click", function click1() {
+    boxClick(crystalValues.gem1);
+    updateScore();
+    isGameOver();
+});
+$("#box2").on("click", function click2() {
+    boxClick(crystalValues.gem2);
+    updateScore();
+    isGameOver();
+});
+$("#box3").on("click", function click3() {
+    boxClick(crystalValues.gem3);
+    updateScore();
+    isGameOver();
+});
+$("#box4").on("click", function click4() {
+    boxClick(crystalValues.gem4);
+    updateScore();
+    isGameOver();
+});
